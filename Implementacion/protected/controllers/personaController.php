11b69390c8ec922fb114f4e1801e73912ee64ff7
dmhoyosc <dmhoyosc@outlook.com>
2014-11-03 03:45:52 -0500

    Visualizar Persona
    Implementación de la secuencia alternativa A1 del caso de uso visualizar
    información.

--- Implementacion/protected/controllers/personaController.php
+++ Implementacion/protected/controllers/personaController.php
@@ -53,6 +53,17 @@ class personaController extends Controller
 	}
 
 	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+
+	/**
 	 * Declares class-based actions.
 	 */
 	public function actions()
@@ -69,6 +80,32 @@ class personaController extends Controller
 				'class'=>'CViewAction',
 			),
 		);
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$dataProvider=new CActiveDataProvider('Persona');
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
+	}
+
+		/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return Persona the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($id)
+	{
+		$model=Persona::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
 	}
 
 }
--- Implementacion/protected/views/persona/view.php
+++ Implementacion/protected/views/persona/view.php
@@ -2,38 +2,187 @@
 /* @var $this PersonaController */
 /* @var $model Persona */
 
+$this->pageTitle=Yii::app()->name . '- '.$model->dni;
 $this->breadcrumbs=array(
 	'Personas'=>array('index'),
 	$model->dni,
-);
+);?>
+<div class="container">
 
-$this->menu=array(
-	array('label'=>'List Persona', 'url'=>array('index')),
-	array('label'=>'Create Persona', 'url'=>array('create')),
-	array('label'=>'Update Persona', 'url'=>array('update', 'id'=>$model->dni)),
-	array('label'=>'Delete Persona', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->dni),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Persona', 'url'=>array('admin')),
-);
-?>
+	<?php /* $this->widget('zii.widgets.CDetailView', array(
+		'data'=>$model,
+		'attributes'=>array(
+			'dni',
+			'nombre',
+			'primerApellido',
+			'segundoApellido',
+			'nombreBibliografico',
+			'fechaNacimiento',
+			'direccionR',
+			'direccionP',
+			'usuario',
+			'tipoDocumento',
+			'genero',
+			'ciudadExpedicion',
+			'ciudadNacimiento',
+			'organizacion',
+		),
+	));*/
+	 ?>
 
-<h1>View Persona #<?php echo $model->dni; ?></h1>
+	<?php $form=$this->beginWidget('CActiveForm', array('id'=>'persona-view', 'enableAjaxValidation'=>true,)); ?>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'dni',
-		'nombre',
-		'primerApellido',
-		'segundoApellido',
-		'nombreBibliografico',
-		'fechaNacimiento',
-		'direccionR',
-		'direccionP',
-		'usuario',
-		'tipoDocumento',
-		'genero',
-		'ciudadExpedicion',
-		'ciudadNacimiento',
-		'organizacion',
-	),
-)); ?>
+	<div class="span12">
+		<h2 align="center">Datos personales</h2>
+	</div>
+	<div class="row-fluid">
+		<div class="span2">
+			
+		</div>
+	 	<div class="span5">
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'tipoDocumento');?></h4>
+				<?php echo $form->label($model, CHtml::value(tipoDocumento::model()->findByPk($model->tipoDocumento),"nombre")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'Numero de identificacion: ');?></h4>
+		 		<?php echo $form->label($model, $model->dni);?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'ciudadExpedicion');?></h4>
+				<?php echo $form->label($model, CHtml::value(ciudad::model()->findByPk($model->ciudadExpedicion),"nombre")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'nombre');?></h4>
+		 		<?php echo $form->label($model, $model->nombre);?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'primerApellido');?></h4>
+		 		<?php echo $form->label($model, $model->primerApellido);?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'segundoApellido');?></h4>
+		 		<?php echo $form->label($model, $model->segundoApellido);?>
+		 	</div>
+		</div>
+		<div class="span5">
+		 	<div>
+		 		<?php if($model->nombreBibliografico!=''){?>
+		 		<h4><?php echo $form->label($model, 'nombreBibliografico');?></h4>
+		 		<?php echo $form->label($model, $model->nombreBibliografico);?>
+		 		<?php }?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'fechaNacimiento');?></h4>
+		 		<?php echo $form->label($model, $model->fechaNacimiento);?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'ciudadNacimiento');?></h4>
+				<?php echo $form->label($model, CHtml::value(ciudad::model()->findByPk($model->ciudadNacimiento),"nombre")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'genero');?></h4>
+				<?php echo $form->label($model, CHtml::value(genero::model()->findByPk($model->genero),"nombre")) ?>
+		 	</div>
+		 	<div>
+		 		<?php if($model->organizacion!=''){?>
+		 		<h4><?php echo $form->label($model, 'organizacion');?></h4>
+		 		<?php echo $form->label($model, $model->organizacion);?>
+		 		<?php }?>
+		 	</div>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span2">
+			
+		</div>
+		<div class="span5">
+	 		<h3>Direccion residencial</h3>
+	 		<div>
+		 		<h4><?php echo $form->label($model, 'ciudad');?></h4>
+				<?php echo $form->label($model, CHtml::value(ciudad::model()->findByPk(CHtml::value(direccionR::model()->findByPk($model->direccionR),"ciudad")),"nombre")) ?>
+		 	</div>
+	 		<div>
+		 		<h4><?php echo $form->label($model, 'direccion');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"direccion")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'barrio');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"barrio")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'telefonoFijo');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"telFijo")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'telefonoMovil');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"telMovil")) ?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"apartadoPostal")!=''){?>
+		 		<h4><?php echo $form->label($model, 'apartadoPostal');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"apartadoPostal")) ?>
+				<?php }?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"codPostal")!=''){?>
+		 		<h4><?php echo $form->label($model, 'codigoPostal');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"codPostal")) ?>
+				<?php }?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'email');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"email")) ?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"sitioWeb")!=''){?>
+		 		<h4><?php echo $form->label($model, 'sitioWeb');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionR::model()->findByPk($model->direccionR),"sitioWeb")) ?>
+				<?php }?>
+		 	</div>
+
+		</div>
+		<div class="span5">
+			<h3>Direccion profesional</h3>
+			<div>
+		 		<h4><?php echo $form->label($model, 'ciudad');?></h4>
+				<?php echo $form->label($model, CHtml::value(ciudad::model()->findByPk(CHtml::value(direccionP::model()->findByPk($model->direccionR),"ciudad")),"nombre")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'direccion');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"direccion")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'barrio');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"barrio")) ?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'telefonoFijo');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"telFijo")) ?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"apartadoPostal")!=''){?>
+		 		<h4><?php echo $form->label($model, 'apartadoPostal');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"apartadoPostal")) ?>
+				<?php }?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"codPostal")!=''){?>
+		 		<h4><?php echo $form->label($model, 'codigoPostal');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"codPostal")) ?>
+				<?php }?>
+		 	</div>
+		 	<div>
+		 		<h4><?php echo $form->label($model, 'email');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"email")) ?>
+		 	</div>
+		 	<div>
+		 		<?php if(CHtml::value(direccionR::model()->findByPk($model->direccionR),"sitioWeb")!=''){?>
+		 		<h4><?php echo $form->label($model, 'sitioWeb');?></h4>
+				<?php echo $form->label($model, CHtml::value(direccionP::model()->findByPk($model->direccionR),"sitioWeb")) ?>
+				<?php }?>
+		 	</div>
+		</div>
+	</div>
+	 <?php $this->endWidget(); ?>
+</div>
