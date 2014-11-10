b18a85b38c12bb7ee53625c6b74869a772c52050
DanielIsaza <danielisaza9@gmail.com>
2014-11-10 13:27:52 -0500

    Creacion del home de los usuaiors

--- Implementacion/protected/views/site/homeUsuario.php
+++ Implementacion/protected/views/site/homeUsuario.php
@@ -7,3 +7,15 @@ $this->breadcrumbs=array(
 <div>
 <?php echo CHtml::submitButton('Visualizar organizaciones', array('submit' => array('organizacion/index'),"class"=>"btn btn-primary btn-large")); ?>
 </div>
+<br/>
+<div>
+	<?php echo CHtml::submitButton('Visualizar grupos', array('submit' => array('grupo/index'),"class"=>"btn btn-primary btn-large")); ?>
+</div>
+<br/>
+<div>
+	<?php echo CHtml::submitButton('Visualizar Personas', array('submit' => array('persona/index'),"class"=>"btn btn-primary btn-large")); ?>
+</div>
+<br/>
+<div>
+	<?php echo CHtml::submitButton('Eventos sugeridos', array('submit' => array('evento/index'),"class"=>"btn btn-primary btn-large")); ?>
+</div>
