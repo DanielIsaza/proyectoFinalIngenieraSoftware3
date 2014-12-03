39e03e38eeca1e088d453d0ae7e4bcedf88d1fdf
dmhoyosc <dmhoyosc@outlook.com>
2014-12-03 12:10:27 -0500

    Mejoramiento del diseño del home
    Mejora de la apariencia del home de usuario

--- Implementacion/protected/views/site/homeUsuario.php
+++ Implementacion/protected/views/site/homeUsuario.php
@@ -5,33 +5,34 @@ $this->breadcrumbs=array(
 );
 ?>
 
-
-
-<div>
-	<?php echo CHtml::submitButton('Sugerir grupos', array('submit' => array('grupo/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br/>
-<div>
-	<?php echo CHtml::submitButton('Sugerir proyectos', array('submit' => array('proyecto/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br/>
-<div>
-	<?php echo CHtml::submitButton('Sugerir eventos', array('submit' => array('evento/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br/>
-
-<div>
-	<?php echo CHtml::submitButton('Visualizar Personas', array('submit' => array('persona/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br/>
-<div>
-<?php echo CHtml::submitButton('Visualizar organizaciones', array('submit' => array('organizacion/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br/>
-<div>
-	<?php echo CHtml::submitButton('Visualizar publicaciones', array('submit' => array('publicacion/index'),"class"=>"btn btn-primary btn-large")); ?>
-</div>
-<br>
-<div>
-<?php echo CHtml::submitButton('Visualizar proyectos', array('submit' => array('proyecto/index'),"class"=>"btn btn-primary btn-large")); ?>
+<div class="container">
+	<div class="panel panel-info">
+		<div class="panel-heading">
+			<h4 align="center">Bienvenido al SIIC</h4>
+			<p align="center">En el sistema, aparte de poder registrar una persona u organización usted puede:</p>
+		</div>
+		<div class="panel-body">
+			<div class="span6">
+				<div class="panel panel-info">
+					<div class="panel-body">
+						<p align="center"><b>Recibir sugerencias de:</b></p>
+						<p align="center"><?php echo CHtml::submitButton('   Grupos    ', array('submit' => array('grupo/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+						<p align="center"><?php echo CHtml::submitButton('Proyectos', array('submit' => array('proyecto/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+						<p align="center"><?php echo CHtml::submitButton('   Eventos   ', array('submit' => array('evento/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+					</div>
+				</div>
+			</div>
+			<div class="span6">
+				<div class="panel panel-info">
+					<div class="panel-body">
+						<p align="center"><b>Ver inforación de:</b></p>
+						<p align="center"><?php echo CHtml::submitButton('       Personas       ', array('submit' => array('persona/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+						<p align="center"><?php echo CHtml::submitButton('Organizaciones', array('submit' => array('organizacion/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+						<p align="center"><?php echo CHtml::submitButton('   Publicaciones  ', array('submit' => array('publicacion/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+						<p align="center"><?php echo CHtml::submitButton('      Proyectos      ', array('submit' => array('proyecto/index'),"class"=>"btn btn-primary btn-large")); ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
